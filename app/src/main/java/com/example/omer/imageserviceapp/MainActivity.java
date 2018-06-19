410097b0b7e107193fe568d01591da5a197e7b01
omer yaso <yasoome@macs.biu.ac.il>
2018-06-19 16:27:15 +0300

    implemented MainActivity

--- app/src/main/java/com/example/omer/imageserviceapp/MainActivity.java
+++ app/src/main/java/com/example/omer/imageserviceapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.omer.imageserviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startService(View view) {
+
+        Intent intent = new Intent(this, ImageService.class);
+        startService(intent);
+
+    }
+
+
+    public void stopService(View  view) {
+        Intent intent = new Intent(this, ImageService.class);
+        stopService(intent);
     }
 }
