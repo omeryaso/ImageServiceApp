55d86631efa30be8c2383e6e06ba828bd8535da7
omer yaso <yasoome@macs.biu.ac.il>
2018-06-19 17:58:46 +0300

    Im pickle ricckkkkk!!

---
+++ app/src/main/java/com/example/omer/imageserviceapp/ImageService.java
@@ -0,0 +1,156 @@
+package com.example.omer.imageserviceapp;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageService extends Service {
+
+    //members
+    IntentFilter intentFilter= new IntentFilter();;
+    BroadcastReceiver broadcastReceiver;
+    List<File> files;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startID) {
+        Toast.makeText(this, "Service starting...", Toast.LENGTH_LONG).show();
+        this.broadcastReceiver = new BroadcastReceiver() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            //start transfer
+                            startTransfer(context);
+                        }
+                    }
+                }
+            }
+        };
+        this.registerReceiver(this.broadcastReceiver, intentFilter);
+        return START_STICKY;
+    }
+
+    /**
+     * onBind - No need to implement so returns NULL.
+     */
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /**
+     * onCreate - .
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.intentFilter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        this.intentFilter.addAction("android.net.wifi.STATE_CHANGE");
+    }
+
+    /**
+     * onDestroy - will be called when  service distraction has started
+     * and will define the proper actions.
+     */
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service ending...", Toast.LENGTH_LONG).show();
+        this.unregisterReceiver(this.broadcastReceiver);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void startTransfer(Context context) {
+        //set notification progress bar
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        final int notifyId = 1;
+        final NotificationManager NM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setContentTitle("Passing images...");
+        builder.setContentText("Passing in progress...");
+        //start the transfer
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int barState = 0;
+                    updatePicsFilesList();
+                    for (File file : files) {
+                        //crete new tcp client to talk with server
+                        TcpClient tcpClient = new TcpClient(file);
+                        //talk to image service and send him the photo
+                        tcpClient.startCommunication();
+                        //update the progress bar
+                        barState = barState + 100 / files.size();
+                        builder.setProgress(100, barState, false);
+                        NM.notify(notifyId, builder.build());
+
+                    }
+                    //finish
+                    builder.setProgress(0, 0, false);
+                    builder.setContentTitle("Finished transfer!");
+                    builder.setContentText("Finished transfer!");
+                    NM.notify(notifyId, builder.build());
+                } catch (Exception ex) {
+
+                }
+            }
+        }).start();
+    }
+
+
+    public void getOneFile(File dir, List<File> picsFilesList) {
+        File[] dirFiles = dir.listFiles();
+        int len = dirFiles.length;
+        for (int i=0; i <len; i++) {
+            if (dirFiles[i].isDirectory()) {
+                getOneFile(dirFiles[i], picsFilesList);
+            } else if(dirFiles[i].toString().contains(".jpg")) {
+                picsFilesList.add(dirFiles[i]);
+            }
+        }
+    }
+
+
+    public void updatePicsFilesList() {
+        File dcim = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        //get the dirs
+        File[] fileOrDir = dcim.listFiles();
+        List<File> picsFilesList = new ArrayList<File>();
+        int len =fileOrDir.length;
+        if (fileOrDir != null) {
+            for (int i=0; i <len; i++) {
+                //check if dir
+                if (fileOrDir[i].isDirectory()) {
+                    getOneFile(fileOrDir[i], picsFilesList);
+                } else if(fileOrDir[i].toString().contains(".jpg")) { //check if file
+                    picsFilesList.add(fileOrDir[i]);
+                }
+            }
+        }
+        //update the member
+        files = picsFilesList;
+    }
+}
---
+++ app/src/main/java/com/example/omer/imageserviceapp/TcpClient.java
@@ -0,0 +1,81 @@
+package com.example.omer.imageserviceapp;
+
+import android.util.Log;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TcpClient {
+    //member
+    File file;
+
+    /**
+     * constructor.
+     * @param myFile - the file to be sent to server
+     */
+    public TcpClient(File myFile) {
+        file = myFile;
+    }
+
+    /**
+     * startCommunication function.
+     * talks to server.
+     * @throws Exception
+     */
+    public void startCommunication() throws Exception {
+
+        try {
+            //here you must put your computer's IP address.
+            InetAddress serverAddr = InetAddress.getByName("10.0.2.2");
+            try {
+                //create a socket to make the connection with the server
+                Socket socket = new Socket(serverAddr, 7999);
+                OutputStream output = socket.getOutputStream();
+                InputStream input = socket.getInputStream();
+                //write image name to server
+                output.write(file.getName().getBytes());
+                //confirm from server he got the name
+                byte[] confirmation = new byte[1];
+                //write image to server
+                if (input.read(confirmation) == 1) {
+                    output.write(extractBytes(file));
+                }
+                output.flush();
+            } catch (Exception e) {
+                Log.e("TCP", "S: Error", e);
+            } finally {
+                //socket.close();
+            }
+        } catch (Exception e) {
+            Log.e("TCP", "C: Error", e);
+        }
+    }
+
+    /**
+     * extractBytes function.
+     * extract bytes from photo.
+     * @param file - photo.
+     * @return byte array
+     * @throws IOException in case of error
+     */
+    public static byte[] extractBytes(File file) throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        FileInputStream fis = new FileInputStream(file);
+        try {
+            int i;
+            while ((i = fis.read(buffer)) != -1) {
+                stream.write(buffer, 0, i);
+            }
+
+        } catch (IOException ex) {
+        }
+        return stream.toByteArray();
+
+    }
+}
